dd737d1442c00d523aade940807f93c085b15e8d
lukean13 <39181664+lukean13@users.noreply.github.com>
2022-08-07 22:38:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 443
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    //res.render('index')
-    res.send("Test Success")
+    res.render('index')
+    //res.send("Test Success")
 })
 
 
