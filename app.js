6b13348a3818c26dcfb2cf8f0dc75aca7870eca6
lukean13 <39181664+lukean13@users.noreply.github.com>
2022-08-08 04:12:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8888
 
+//use this for parsing post body
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
